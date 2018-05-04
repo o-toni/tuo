5e49d9bd8b2417776129ed8f04aa70db25b362d5
tee <adetooni@gmail.com>
2018-05-04 23:47:25 +0100

    Initial commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Dashboard</div>
+
+                <div class="panel-body">
+                    <h3>Your Blog Posts</h3>
+                    <a href="/posts/create" class="btn btn-primary">Create Post</a>
+                    <hr>
+                    @if(count($posts) > 0)
+                    <table class="table table-striped">
+                        <tr>
+                            <td>Title</td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        @foreach($posts as $post)
+                        <tr>
+                            <td>{{$post->title}}</td>
+                            <td><a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a></td>
+                            <td>
+                                    {!! Form::open(['action' => ['PostsController@destroy',$post->id], 'method' => 'POST' ,'class' => 'pull-right']) !!}
+        
+                                        {!! Form::hidden('_method', 'DELETE') !!}
+                                        {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+                                    
+                                    {!! Form::close() !!}
+                            </td>
+                        </tr>
+                        @endforeach
+                    </table>
+                    @else 
+                        <p> You Have no Posts</p>
+                    @endif
+                    <hr>
+                    @if (session('status'))
+                        <div class="alert alert-success">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    You are logged in!
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+@section('content')
+<a href="/posts" class="btn btn-default">Go Back</a>
+    <h1>{{$post->title}}</h1>
+    <br>
+    <br>
+    <img style="width:100%" src="/storage/cover_images/{{$post->cover_image}}">
+    <br>
+    <br>
+    <div>
+        {!!$post->body!!}
+    </div>
+    <hr>
+    <small>Written on {{$post->created_at}} By {{$post->User->name}}</small>
+    <br>
+    <br>
+    @if(!Auth::guest())
+        @if(Auth::User()->id == $post->user_id)
+    <a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a>
+    
+    {!! Form::open(['action' => ['PostsController@destroy',$post->id], 'method' => 'POST' ,'class' => 'pull-right']) !!}
+        
+        {!! Form::hidden('_method', 'DELETE') !!}
+        {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+        
+    {!! Form::close() !!}
+        @endif
+    @endif
+    
+@endsection
